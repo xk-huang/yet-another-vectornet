553ab22c8d810db639c86a2add87858601ad8a4a
Yinqian_Lab <xiaokehuang@foxmail.com>
2020-06-21 19:17:43 +0800

    fix bug: lr not decay & add save_checkpoint

--- train.py
+++ train.py
@@ -35,11 +35,37 @@ in_channels, out_channels = 8, 60
 show_every = 10
 val_every = 1
 small_dataset = False
+end_epoch = 0
+save_dir = 'trained_params'
+best_minade = float('inf')
 # eval related
 max_n_guesses = 1
 horizon = 30
 miss_threshold = 2.0
 
+
+#%%
+def save_checkpoint(checkpoint_dir, model, optimizer, end_epoch, val_minade):
+    # state_dict: a Python dictionary object that:
+    # - for a model, maps each layer to its parameter tensor;
+    # - for an optimizer, contains info about the optimizerâ€™s states and hyperparameters used.
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    state = {
+        'state_dict': model.state_dict(),
+        'optimizer' : optimizer.state_dict(),
+        'end_epoch' : end_epoch,
+        'val_minade': val_minade
+        }
+    checkpoint_path = os.path.join(checkpoint_dir, f'epoch_{end_epoch}.valminade_{val_minade:.3f}.pth')
+    torch.save(state, checkpoint_path)
+    print('model saved to %s' % checkpoint_path)
+    
+def load_checkpoint(checkpoint_path, model, optimizer):
+    state = torch.load(checkpoint_path)
+    model.load_state_dict(state['state_dict'])
+    optimizer.load_state_dict(state['optimizer'])
+    print('model loaded from %s' % checkpoint_path)
+    return checkpoint_path['end_epoch']
 #%%
 if __name__ == "__main__":
     np.random.seed(SEED)
@@ -65,6 +91,9 @@ if __name__ == "__main__":
     model.train()
     for epoch in range(epochs):
         acc_loss = .0
+        scheduler.step()
+        if epoch < end_epoch:
+            continue
         for data in train_loader:
             data = data.to(device)
             y = data.y.view(-1, out_channels).to(device)
@@ -77,13 +106,18 @@ if __name__ == "__main__":
         print(
             f"loss at epoch {epoch}: {acc_loss / len(train_loader):.3f}, lr{optimizer.state_dict()['param_groups'][0]['lr']: .3f}")
         if (epoch+1) % val_every == 0:
-            get_eval_metric_results(model, val_loader, device, out_channels, max_n_guesses, horizon, miss_threshold)
-
+            metrics = get_eval_metric_results(model, val_loader, device, out_channels, max_n_guesses, horizon, miss_threshold)
+            curr_minade = metrics["minADE"]
+            if curr_minade < best_minade:
+                best_minade = curr_minade
+                save_checkpoint(save_dir, model, optimizer, epoch, best_minade)
 
     # eval result on the identity dataset
-    get_eval_metric_results(model, val_loader, device, out_channels, max_n_guesses, horizon, miss_threshold)
-
-
+    metrics = get_eval_metric_results(model, val_loader, device, out_channels, max_n_guesses, horizon, miss_threshold)
+    curr_minade = metrics["minADE"]
+    if curr_minade < best_minade:
+        best_minade = curr_minade
+        save_checkpoint(save_dir, model, optimizer, -1, best_minade)
     # model.eval()
     # from utils.viz_utils import show_pred_and_gt
     # with torch.no_grad():
